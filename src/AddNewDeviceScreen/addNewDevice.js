7d0520a2bbe8589f25857893391143ffc6b4ab94
rohit-karthik <63020497+rohit-karthik@users.noreply.github.com>
2021-10-10 18:18:28 -0700

    add next page of new device

--- src/AddNewDeviceScreen/addNewDevice.js
+++ src/AddNewDeviceScreen/addNewDevice.js
@@ -1,13 +1,20 @@
 import React, { useEffect, useCallback } from 'react'
-import { ScrollView, Pressable, Image, Dimensions, TouchableOpacity, View, StatusBar, Appearance , TextInput} from 'react-native';
+import { ScrollView, Pressable, Image, Dimensions, TouchableOpacity, View, StyleSheet, Appearance, TextInput } from 'react-native';
 import { CustomText } from '../customText'
 import { activeDeviceStyles } from '../styles.js';
 import axios from 'axios';
 import { useGlobalUsername } from '../currentUserName';
-import Slider from '@react-native-community/slider'
+import Slider from '@react-native-community/slider';
 import { styles } from '../styles.js';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, DefaultTheme, DarkTheme, getFocusedRouteNameFromRoute } from '@react-navigation/native';
+
+const localStyles = StyleSheet.create({
+    nextFillButton: {
+        ...styles.fillButton,
+        backgroundColor: '#00E391'
+    }
+})
 
 const Stack = createStackNavigator();
 
@@ -23,12 +30,29 @@ async function setWifiForDevice(ssid, psk) {
     await axios.post(url, postData)
 }
 
+async function registerNewDevice(deviceName, username) {
+    const url = "http://192.168.1.140:8000/register-device"
+    const postData = {
+        "new_device_name": deviceName,
+        "username": /*username*/ "rohit"
+    }
+
+    const response = await axios.post(url, postData)
+    const checkData = {
+        "Result": "Failure"
+    }
+
+    if (JSON.stringify(response.data) == JSON.stringify(checkData)) {
+        return "Error: your input is invalid or device already exists."
+    }
+    else {
+        return "Ok"
+    }
+
+}
 export function NewDevice() {
     return (
         <Stack.Navigator screenOptions={{
-            headerStyle: {
-                height: Platform.OS == "ios" ? 80 : StatusBar.currentHeight + 40,
-            },
             headerTitleAlign: 'center',
             headerTitleStyle: {
                 fontFamily: 'Manrope_500Medium'
@@ -42,6 +66,15 @@ export function NewDevice() {
             }}></Stack.Screen>
             <Stack.Screen name="Page3" component={NewDevicePage3} options={{
                 title: 'Enter your WiFi Details'
+            }}></Stack.Screen>
+            <Stack.Screen name="Page4" component={NewDevicePage4} options={{
+                title: 'Is your device connected?'
+            }}></Stack.Screen>
+            <Stack.Screen name="Page5" component={NewDevicePage5} options={{
+                title: 'Set your device\'s name'
+            }}></Stack.Screen>
+            <Stack.Screen name="Page6" component={NewDevicePage6} options={{
+                title: 'test page'
             }}></Stack.Screen>
         </Stack.Navigator>
     )
@@ -96,15 +129,15 @@ function NewDevicePage2({ navigation }) {
         <ScrollView contentContainerStyle={{ backgroundColor: '#FFFFFF', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Image source={require('../../assets/wifi_icon.png')} style={{ width: 150, height: 150, marginBottom: 10 }} />
             <CustomText style={{ textAlign: 'center', fontSize: 25, marginLeft: 10, marginRight: 10 }}>
-                On your device, connect to your device's hotspot. The password is 
-                    <CustomText style={{ color: '#FE0000' }}>"RedworthHumSpot"</CustomText> 
+                On your device, connect to your device's hotspot. The password is
+                <CustomText style={{ color: '#FE0000' }}>"RedworthHumSpot"</CustomText>
                 (no quotes).{'\n\n'}
                 The name should look like: <CustomText style={{ color: '#FE0000' }}>Redworth-HUM-Spot</CustomText> {'\n\n'}
                 Once connected, hit "Next".</CustomText>
             <View style={styles.contentMargin}>
                 <TouchableOpacity
                     style={styles.fillButton}
-                    onPress={() => navigation.navigate('Page3')}
+                    onPress={() => /*navigation.navigate('Page3')*/ navigation.navigate('Page4')}
                 >
                     <View style={{ flexDirection: 'row' }}>
                         <CustomText style={[styles.fillButtonText, { alignSelf: 'flex-start', fontSize: 30, width: '90%' }]}>Next</CustomText>
@@ -122,7 +155,7 @@ function NewDevicePage3({ navigation }) {
     var [psk, changePSK] = React.useState("")
 
     return (
-        <View style={{ backgroundColor: '#FFFFFF', flex: 1, justifyContent: 'center'}}>
+        <View style={{ backgroundColor: '#FFFFFF', flex: 1, justifyContent: 'center' }}>
             <Image source={require('../../assets/wifi_icon.png')} style={{ width: 150, height: 150, marginBottom: 10, alignSelf: 'center' }} />
             <CustomText style={{ textAlign: 'center', fontSize: 25, marginLeft: 10, marginRight: 10 }}>
                 Enter your SSID and Password to connect your device to your home WiFi network.
@@ -150,8 +183,8 @@ function NewDevicePage3({ navigation }) {
                 <TouchableOpacity
                     style={styles.fillButton}
                     onPress={async () => {
-                        navigation.navigate('Page3')
                         await setWifiForDevice(ssid, psk)
+                        navigation.navigate("Page4")
                     }}
                 >
                     <View style={{ flexDirection: 'row' }}>
@@ -161,5 +194,108 @@ function NewDevicePage3({ navigation }) {
                 </TouchableOpacity>
             </View>
         </View>
+    )
+}
+
+function NewDevicePage4({ navigation }) {
+    return (
+        <ScrollView contentContainerStyle={{ backgroundColor: '#FFFFFF', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <CustomText style={{ textAlign: 'center', fontSize: 35, marginLeft: 10, marginRight: 10, marginBottom: 10 }}>
+                Did your device connect?
+            </CustomText>
+            <CustomText style={{ textAlign: 'center', fontSize: 25, margin: 10 }}>
+                If your device successfully connected, the hotspot should be gone, and the LED should show
+                <CustomText style={{ color: '#00E391' }}> green</CustomText>. Tap "Next" to move on!
+            </CustomText>
+            <CustomText style={{ textAlign: 'center', fontSize: 25, margin: 10 }}>
+                If not, the LED should show
+                <CustomText style={{ color: '#FE0000' }}> red</CustomText>. Try again by hitting the "Retry" button below.
+            </CustomText>
+            <View style={{ display: 'flex', flexDirection: 'row' }}>
+                <View style={styles.contentMargin}>
+                    <TouchableOpacity
+                        style={styles.fillButton}
+                        onPress={() => navigation.navigate('Page2')}
+                    >
+                        <View style={{ flexDirection: 'row' }}>
+                            <CustomText style={[styles.fillButtonText, { alignSelf: 'flex-start', fontSize: 30 }]}>Retry</CustomText>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.contentMargin}>
+                    <TouchableOpacity
+                        style={localStyles.nextFillButton}
+                        onPress={() => navigation.navigate('Page5')}
+                    >
+                        <View style={{ flexDirection: 'row' }}>
+                            <CustomText style={[styles.fillButtonText, { alignSelf: 'flex-start', fontSize: 30 }]}>Next</CustomText>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </ScrollView>
+    )
+}
+
+function NewDevicePage5({ navigation }) {
+
+    var [deviceName, changeName] = React.useState("")
+    var [errorShown, setErrorShown] = React.useState(false);
+    var [registerMessage, changeRegisterMessage] = React.useState("")
+    const username = useGlobalUsername();
+
+    return (
+        <View style={{ backgroundColor: '#FFFFFF', flex: 1, justifyContent: 'center' }}>
+            <Image source={require('../../assets/humidifier.png')} style={{ width: 200, height: 200, marginBottom: 10, alignSelf: 'center' }} />
+            <CustomText style={{ textAlign: 'center', fontSize: 25, marginLeft: 10, marginRight: 10 }}>
+                Enter your new device's name.
+            </CustomText>
+            <View style={styles.contentMargin}>
+                <TextInput
+                    style={styles.textInput}
+                    onChangeText={changeName}
+                    value={deviceName}
+                    placeholder=" Device Name"
+                    placeholderTextColor='#000000'
+                />
+            </View>
+            <View style={styles.contentMargin}>
+                <TouchableOpacity
+                    style={styles.fillButton}
+                    onPress={async () => {
+                        var res = await registerNewDevice(deviceName, username)
+                        if (res != "Ok") {
+                            setErrorShown(true)
+                            changeRegisterMessage(res)
+                        }
+                        else {
+                            setErrorShown(false)
+                            navigation.navigate('Page6')
+                        }
+                    }}
+                >
+                    <View style={{ flexDirection: 'row' }}>
+                        <CustomText style={[styles.fillButtonText, { alignSelf: 'flex-start', fontSize: 30, width: '90%' }]}>Next</CustomText>
+                        <CustomText style={[styles.fillButtonText, { fontSize: 30, justifyContent: 'flex-start', alignSelf: 'center', width: '10%' }]}>{'->'}</CustomText>
+                    </View>
+                </TouchableOpacity>
+            </View>
+            {
+                errorShown ? (
+                    <View style={styles.contentMargin}>
+                        <CustomText style={{ alignSelf: 'center', color: "#ff0000", fontSize: 18, textAlign: 'center' }}>
+                            {registerMessage}
+                        </CustomText>
+                    </View>
+                ) : null
+            }
+        </View>
+    )
+}
+
+function NewDevicePage6({ navigation }) {
+
+    return (
+        <View style={{ backgroundColor: '#FFFFFF', flex: 1, justifyContent: 'center' }}></View>
     )
 }
